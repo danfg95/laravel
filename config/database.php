ee91fcef313a4f1505ea51ab451d1c970641ac75
Daniel <danfg95@gmail.com>
2020-03-06 16:02:16 +0100

    Changed config/database.php the new database storage

--- config/database.php
+++ config/database.php
@@ -42,15 +42,14 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'your_database_name from_db4free'),
+            'username' => env('DB_USERNAME', 'your_db4free_      username'),
+            'password' => env('DB_PASSWORD', 'your_db4free_password'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
